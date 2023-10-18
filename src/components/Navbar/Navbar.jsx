17f90f591eb0ec211795266d1cb3a9c252e02e54
sattamap <sattamap@gmail.com>
2023-10-18 15:27:53 +0600

    Create Navbar component without routes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,68 @@
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
     return (
         <div>
-          
+           <div className="max-w-[1300px] mx-auto navbar py-14">
+            <div className="flex-1">
+                <img  src="" alt="" />
+                <h1 className="text-2xl font-extrabold">Tech<span className=" text-emerald-600">Fleet</span></h1>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li>
+                        <NavLink
+                            to="/"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending font-normal" : isActive ? "text-[#FF444A] font-bold underline" : ""
+                            }
+                        >
+                            Home
+                        </NavLink>
+
+                    </li>
+
+                    <li>
+                        <NavLink
+                            to="/product"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending font-normal" : isActive ? "text-[#FF444A] font-bold underline" : ""
+                            }
+                        >
+                            Add Product
+                        </NavLink>
+
+
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/cart"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending font-normal" : isActive ? "text-[#FF444A] font-bold underline" : ""
+                            }
+                        >
+                            My Cart
+                        </NavLink>
+
+
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/login"
+                            className={({ isActive, isPending }) =>
+                                isPending ? "pending font-normal" : isActive ? "text-[#FF444A] font-bold underline" : ""
+                            }
+                        >
+                            Login
+                        </NavLink>
+
+
+                    </li>
+                </ul>
+
+            </div>
+        </div>
             
         </div>
     );
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,11 @@
+import Navbar from "../components/Navbar/Navbar";
 
 
 const MainLayout = () => {
     return (
         <div>
-            <h2>My Layout</h2>
+            
+            <Navbar></Navbar>
         </div>
     );
 };
