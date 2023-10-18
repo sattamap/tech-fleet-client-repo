1e3a60292f111db38fe777e157fc771a3d5510be
sattamap <sattamap@gmail.com>
2023-10-19 03:37:13 +0600

    Add ProtectedRoutes component for page access control

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import MyCart from './pages/MyCart/MyCart';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import AuthProvider from './provider/AuthProvider';
+import ProtectedRoutes from './ProtectedRoutes/ProtectedRoutes';
 
 const router = createBrowserRouter([
   {
@@ -27,11 +28,11 @@ const router = createBrowserRouter([
       },
       {
           path: "/product",
-          element: <AddProduct></AddProduct>,
+          element: <ProtectedRoutes><AddProduct></AddProduct></ProtectedRoutes>,
       },
       {
           path: "/cart",
-          element:<MyCart></MyCart>,
+          element:<ProtectedRoutes><MyCart></MyCart></ProtectedRoutes>,
           
       },
       {
