0df770448810b954dce8b004a93bc3bf2851ff3a
sattamap <sattamap@gmail.com>
2023-10-21 22:18:19 +0600

    Replace vercel server link with my local server link

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,8 @@
 const ErrorPage = () => {
     return (
         <div className='h-screen flex justify-center  items-center'>
-            <h1 className="font-extrabold text-5xl">Opps!!</h1>
+            <h1 className="font-extrabold text-5xl">Oops!!</h1>
+            <p className="mt-4 text-xl text-red-500">Page not found.</p>
         </div>
     );
 };
